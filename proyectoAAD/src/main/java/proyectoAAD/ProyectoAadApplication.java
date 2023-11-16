fae65542950b892de8c789f5dacbff0f35fba9b2
Kafrita-97 <130869427+Kafrita-97@users.noreply.github.com>
2023-11-17 00:13:31 +0100

    cosas menores

--- proyectoAAD/src/main/java/proyectoAAD/ProyectoAadApplication.java
+++ proyectoAAD/src/main/java/proyectoAAD/ProyectoAadApplication.java
@@ -8,18 +8,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ProyectoAadApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoAadApplication.class, args);
-		
 		xmlTreatment file_1 = new xmlTreatment("src/main/resources/contratos-adjudicados-jun-22.xml");
 		file_1.readXML();
-
+		
 		
 		OpenBrowser op = new OpenBrowser();
-        try {
+		try {
 			op.abrirNavegadorPorDefecto("http://localhost:8080/h2-console/");
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
 	}
-
+	
 }
+
