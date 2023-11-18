6021682b284bb76707a94fb755eafc1f07e10af5
Kafrita-97 <130869427+Kafrita-97@users.noreply.github.com>
2023-11-18 02:37:03 +0100

    .

--- proyectoAAD/src/main/java/proyectoAAD/ProyectoAadApplication.java
+++ proyectoAAD/src/main/java/proyectoAAD/ProyectoAadApplication.java
@@ -7,10 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProyectoAadApplication {
 	public static void main(String[] args) {
-		
-		xmlCleaner file1 = new xmlCleaner("", "/Users/juanma/Library/Mobile Documents/com~apple~CloudDocs/V/Estudios/DAM/2º/Acceso a datos/Trabajo voluntario - Acceso a datos/Proyecto/proyectoAAD/src/main/resources/contratos-adjudicados-jun-22.xml");
-		
 		SpringApplication.run(ProyectoAadApplication.class, args);
+		
 		/* OpenBrowser op = new OpenBrowser();
 		try {
 			op.abrirNavegadorPorDefecto("http://localhost:8080/h2-console/");
